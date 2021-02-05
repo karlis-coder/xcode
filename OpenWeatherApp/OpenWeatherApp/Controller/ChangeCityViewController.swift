8a7fc86f5b33d13681d5ca3039650a22af5bf33f
Karlis B <karlis.butins@gmail.com>
2021-02-05 15:27:54 +0200

    countrylist app

---
+++ OpenWeatherApp/OpenWeatherApp/Controller/ChangeCityViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChangeCityViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Arkadijs Makarenko on 04/02/2021.
+//
+
+import UIKit
+
+class ChangeCityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ OpenWeatherApp/OpenWeatherApp/Controller/WeatherViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WeatherViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Arkadijs Makarenko on 04/02/2021.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
